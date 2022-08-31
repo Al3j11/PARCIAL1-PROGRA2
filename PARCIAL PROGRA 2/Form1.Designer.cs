87fddf6b6d61dfc1f5ae382f765f0c8a5a8afc99
Al3j11 <pasalejandro9797@gmail.com>
2022-08-31 01:02:08 -0600

    Creación de interfaz

--- PARCIAL PROGRA 2/Form1.Designer.cs
+++ PARCIAL PROGRA 2/Form1.Designer.cs
@@ -30,6 +30,18 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PARCIAL1));
             this.btnSalir = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.textColumnasM2 = new System.Windows.Forms.TextBox();
+            this.textFilasM2 = new System.Windows.Forms.TextBox();
+            this.textColumnasM1 = new System.Windows.Forms.TextBox();
+            this.textFilasM1 = new System.Windows.Forms.TextBox();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSalir
@@ -41,13 +53,139 @@
             this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnSalir.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.btnSalir.Location = new System.Drawing.Point(971, 34);
+            this.btnSalir.Location = new System.Drawing.Point(-2, 704);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(144, 41);
+            this.btnSalir.Size = new System.Drawing.Size(200, 41);
             this.btnSalir.TabIndex = 0;
             this.btnSalir.Text = "SALIR";
             this.btnSalir.UseVisualStyleBackColor = false;
             this.btnSalir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Transparent;
+            this.panelMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMenu.Controls.Add(this.label4);
+            this.panelMenu.Controls.Add(this.label3);
+            this.panelMenu.Controls.Add(this.label2);
+            this.panelMenu.Controls.Add(this.label1);
+            this.panelMenu.Controls.Add(this.textColumnasM2);
+            this.panelMenu.Controls.Add(this.textFilasM2);
+            this.panelMenu.Controls.Add(this.textColumnasM1);
+            this.panelMenu.Controls.Add(this.textFilasM1);
+            this.panelMenu.Controls.Add(this.btnMultiplicar);
+            this.panelMenu.Controls.Add(this.btnAgregar);
+            this.panelMenu.Controls.Add(this.btnSalir);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelMenu.Location = new System.Drawing.Point(1186, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 788);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // textColumnasM2
+            // 
+            this.textColumnasM2.Location = new System.Drawing.Point(-2, 301);
+            this.textColumnasM2.Name = "textColumnasM2";
+            this.textColumnasM2.Size = new System.Drawing.Size(201, 23);
+            this.textColumnasM2.TabIndex = 5;
+            // 
+            // textFilasM2
+            // 
+            this.textFilasM2.Location = new System.Drawing.Point(-1, 244);
+            this.textFilasM2.Name = "textFilasM2";
+            this.textFilasM2.Size = new System.Drawing.Size(201, 23);
+            this.textFilasM2.TabIndex = 4;
+            // 
+            // textColumnasM1
+            // 
+            this.textColumnasM1.Location = new System.Drawing.Point(-1, 117);
+            this.textColumnasM1.Name = "textColumnasM1";
+            this.textColumnasM1.Size = new System.Drawing.Size(201, 23);
+            this.textColumnasM1.TabIndex = 3;
+            // 
+            // textFilasM1
+            // 
+            this.textFilasM1.Location = new System.Drawing.Point(-1, 56);
+            this.textFilasM1.Name = "textFilasM1";
+            this.textFilasM1.Size = new System.Drawing.Size(201, 23);
+            this.textFilasM1.TabIndex = 2;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.Crimson;
+            this.btnMultiplicar.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.btnMultiplicar.FlatAppearance.BorderSize = 3;
+            this.btnMultiplicar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMultiplicar.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnMultiplicar.Location = new System.Drawing.Point(-1, 449);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(200, 41);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "MULTIPLICAR";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Crimson;
+            this.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.btnAgregar.FlatAppearance.BorderSize = 3;
+            this.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAgregar.Location = new System.Drawing.Point(-1, 374);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(200, 41);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(71, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "FILAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(40, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "COLUMNAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(71, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 28);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "FILAS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(40, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "COLUMNAS";
             // 
             // PARCIAL1
             // 
@@ -55,13 +193,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1127, 450);
+            this.ClientSize = new System.Drawing.Size(1386, 788);
             this.ControlBox = false;
-            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.panelMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PARCIAL1";
             this.Text = "PARCIAL1-MATRICES";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,5 +209,16 @@
         #endregion
 
         private Button btnSalir;
+        private Panel panelMenu;
+        private Button btnMultiplicar;
+        private Button btnAgregar;
+        private TextBox textColumnasM2;
+        private TextBox textFilasM2;
+        private TextBox textColumnasM1;
+        private TextBox textFilasM1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
