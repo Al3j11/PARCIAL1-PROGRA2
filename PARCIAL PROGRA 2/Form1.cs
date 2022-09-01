37467e34b84cbb37b959520d209e89373355e70d
Al3j11 <pasalejandro9797@gmail.com>
2022-08-31 23:04:03 -0600

    Cambio

--- PARCIAL PROGRA 2/Form1.cs
+++ PARCIAL PROGRA 2/Form1.cs
@@ -14,50 +14,54 @@ namespace PARCIAL_PROGRA_2
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
+
+       
             textFilasM1.Clear();
             textColumnasM1.Clear();
             textFilasM2.Clear();
             textColumnasM2.Clear();
             textFilasM1.Focus();
         }
+        
+
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-
-            if ((textFilasM1.Text.Trim() == "") || (textColumnasM1.Text.Trim() == "")|| (textColumnasM2.Text.Trim() == "")|| (textFilasM2.Text.Trim() == ""))
+            
+            if ((textFilasM1.Text.Trim() == "") || (textColumnasM1.Text.Trim() == "") || (textColumnasM2.Text.Trim() == "") || (textFilasM2.Text.Trim() == ""))
             {
                 MessageBox.Show("Campos vacios", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
-            else { 
+            else {
 
-            int filas = Convert.ToInt32(textFilasM1.Text);
-            int columnas = Convert.ToInt32(textColumnasM1.Text);
-            int filasB = Convert.ToInt32(textFilasM2.Text);
-            int columnasB = Convert.ToInt32(textColumnasM2.Text);
+                int filas = Convert.ToInt32(textFilasM1.Text);
+                int columnas = Convert.ToInt32(textColumnasM1.Text);
+                int filasB = Convert.ToInt32(textFilasM2.Text);
+                int columnasB = Convert.ToInt32(textColumnasM2.Text);
 
 
-            TextBox[,] textBoxesA, textBoxesB;
-            textBoxesA = new TextBox[filas, columnas];
-            textBoxesB = new TextBox[filasB, columnasB];
+                TextBox[,] textBoxesA, textBoxesB;
+                textBoxesA = new TextBox[filas, columnas];
+                textBoxesB = new TextBox[filasB, columnasB];
 
-            for (int x = 0; x < filas; x++)
-            {
-                for (int y = 0; y < columnas; y++)
+                for (int x = 0; x < filas; x++)
                 {
-                    textBoxesA[x, y] = new TextBox();
-                    textBoxesA[x, y].Text = "";
-                    textBoxesA[x, y].Width = 50;
-                    textBoxesA[x, y].Location = new Point(200 + 100 * y, 200 + 50 * x);
-                    textBoxesA[x, y].TabIndex = 100 + x + 2 * y;
+                    for (int y = 0; y < columnas; y++)
+                    {
+                        textBoxesA[x, y] = new TextBox();
+                        textBoxesA[x, y].Text = "";
+                        textBoxesA[x, y].Width = 50;
+                        textBoxesA[x, y].Location = new Point(200 + 100 * y, 200 + 50 * x);
+                        textBoxesA[x, y].TabIndex = 100 + x + 2 * y;
 
-                    Controls.Add(textBoxesA[x, y]);
+                        Controls.Add(textBoxesA[x, y]);
 
+
+                    }
 
                 }
-
-            }
-            for (int x = 0; x < filasB; x++)
-            {
+                for (int x = 0; x < filasB; x++)
+                {
                     for (int y = 0; y < columnasB; y++)
                     {
                         textBoxesB[x, y] = new TextBox();
@@ -67,10 +71,17 @@ namespace PARCIAL_PROGRA_2
                         textBoxesB[x, y].TabIndex = 100 + x + 2 * y;
 
                         Controls.Add(textBoxesB[x, y]);
+
+                       
+
+
                     }
 
+                    
                 }
             }
+           
         }
+       
     }
-}
+    }
